73d1eba556599b15846ba08a18b8b39509108b19
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-09-23 12:36:32 +0100

    consistent column naming

--- src/services/reporting-data-service.js
+++ src/services/reporting-data-service.js
@@ -31,7 +31,7 @@ const CSV_FILES = {
     'Option_value'
   ],
   transactional: ['Reference', 'Status', 'Event_dt', 'User_id'],
-  parcels: ['Agreement_ID', 'parcel_reference']
+  parcels: ['Agreement_ID', 'Parcel_reference']
 }
 
 const AGREEMENT_STATUS_INDEX = 3
